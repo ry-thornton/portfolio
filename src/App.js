291be9ea76145dc69b20f5e4eacefcca149405b3
github username <48257792+ry-thornton@users.noreply.github.com>
2020-09-08 18:07:09 -0400

    revised code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import ProfileCard from './ProfileCard.js'
 import './App.css';
 
 function App() {
@@ -27,28 +28,7 @@ function App() {
           <div className="columns">
             <div className="column">
               <br/>
-              <div className="box">
-                <img id="avatar" src="profilepicture.jpg"/>
-                <br/>
-                <h1 className="title">Ryan Thornton</h1>
-                software engineer 🛠,
-                front-end developer 💻,
-                <a href="https://rythornton.com" target="_blank"> photographer</a> 📷
-                <br/>
-                <br/>
-                <div className="buttons">
-                  <span className="tag button">ReactJS</span>
-                  <span className="tag button">HTML</span>
-                  <span className="tag button">Javascript</span>
-                  <span className="tag button">CSS</span>
-                  <span className="tag button">Ruby on Rails</span>
-                  <span className="tag button">Bulma</span>
-                  <span className="tag button">REST APIs</span>
-                  <span className="tag button">Firebase</span>
-                  <span className="tag button">Object Oriented Programming</span>
-                  <span className="tag button">Wireframing</span>
-                </div>
-              </div>
+              <ProfileCard/>
             </div>
             <div className="column">
 
---
+++ src/ProfileCard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function ProfileCard(props){
+
+    let skills = [
+        "ReactJS",
+        "HTML",
+        "Javascript",
+        "CSS",
+        "Ruby on Rails",
+        "Bulma",
+        "REST APIs",
+        "Firebase",
+        "Object Oriented Programming",
+        "Wireframing",
+        "always learning more..."
+    ]
+
+    // let renderSkills = () => {
+    //     skills.map(skill => <span className="tag button">{skill}</span>)
+    // }
+
+    return (
+        <>
+            <div className="box">
+                <img id="avatar" src="profilepicture.jpg"/>
+                <br/>
+                <h1 className="title">Ryan Thornton</h1>
+                software engineer 🛠,
+                front-end developer 💻,
+                <a href="https://rythornton.com" target="_blank"> photographer</a> 📷
+                <br/>
+                <br/>
+                <div className="buttons">
+                    {skills.map(skill => <span className="tag button">{skill}</span>)}
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default ProfileCard
