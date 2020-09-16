14d753087b7ed7e2f3599a4547e8607a1568c256
github username <48257792+ry-thornton@users.noreply.github.com>
2020-09-15 20:39:17 -0400

    added videos to projects

--- src/Projects.js
+++ src/Projects.js
@@ -22,14 +22,14 @@ class Projects extends React.Component{
         //based on keyword filter projects in state that include the
         //keyword and render them to the screen in Project Cards
         let filteredProjects = this.state.projects.filter(project => project.includes(this.state.keyword))
-        return filteredProjects.map(project => <span className="box Project-card">{project}</span>)
+        return filteredProjects.map(project => <span className="box Project-card"><video width="100%" controls><source src={`${project}.mov`}></source></video><br/>{project}</span>)
     }
 
     render(){
         return (
             <>
             {/* Container for project search bar/filter and all projects (cards)*/}
-            <SearchForm changeHandler={this.changeHandler} keyword={this.state.keyword}/>
+            {/* <SearchForm changeHandler={this.changeHandler} keyword={this.state.keyword}/> */}
             <br/>
             Projects
             <div id="project-container">
